e78b33005eabbfa9ee689902380779e87826548a
mjnicolas <78150839+mjnicolas@users.noreply.github.com>
2021-01-30 21:47:40 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
+nestedList=[["PRODUCT ID", "PRODUCT NAME","CATEGORY", "STOCKS", "PRICE"],
+            ["S0001","WH-CH510 Wireless Headphone","Headphones","10","2,999.00"],
+            ["S0002","MDR-XB55AP EXTRA BASS","Headphones","3","1,499.00"],
+            ["S0003","XB550AP EXTRA BASS","Headphones","8","2,999.00"],
+            ["S0004","W830 Compact Camera","Camera","10","6,999.00"],
+            ["S0005","H300 Camera","Camera","7","12,999.00"],
+            ["S0006","XB01 EXTRA BASS","Speaker","15","1,999.00"],
+            ["S0007","GTK-PG10 Outdoor Speaker","Speaker","2","12,999.00"],
+            ["S0008","XB32 EXTRA BASS","Speaker","12","6,799.00"],
+            ["S0009","MDR-E9LP In-ear","Earphones","15","499.00"],
+            ["S0010","MDR-XB55AP In-ear","Earphones","9","1,499.00"],
+            ["S0011","WI-SP51 Wireless In-ear","Earphones","6","4,299.00"],
+            ["S0012","WS410 Walkman","MP3 Player","3","4,999.00"],
+            ["S0013","A50 Walkman","MP3 Player","5","9,999.00"],
+            ["S0014","WS620 Walkman","MP3 Player","5","6,999.00"],
+            ["S0015","WF-XB700 Truly Wireless","Headphones","6","6,799.00"],
+            ["S0016","XPeria XA","Cellphone","14","10,690.00"],
+            ["S0017","XPeria X","Cellphone","10","15,890.00"],
+            ["S0018","XPeria XA2 Ultra","Cellphone","10","24,490.00"],
+            ["S0019","XPeria XA1","Cellphone","13","14,890.00"],
+            ["S0020","XPeria XA1 Ultra","Cellphone","3","19,090.00"],]
+
 # nested list of admin usernames and their passwords
 admin_users = [
     ["admin_name", "1234"]
