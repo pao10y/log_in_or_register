eaf4613611ef61efd641518b525ad19eb423eed2
krtjsh <78158293+krtjsh@users.noreply.github.com>
2021-02-01 07:12:13 +0800

    add products

---
+++ inbound6115156156481867187.py
@@ -0,0 +1,22 @@
+def Add_product():
+    verify = 0
+    res = "Y" or "y"
+    prodList()
+    while True:
+        n=input("Enter name of product:  ")
+        num=int(input("Enter product id:  "))
+        c=input("Enter category:  ")
+        s=int(input("Enter stocks:    "))
+        p=int(input("Enter price:"))
+        print("current list",product)
+        print("Item",[n],[num],[c],[s],[p])
+        resp=input("do you want to add this item? [Y/N]:  ")
+        if resp.lower() == "Y" or "y":
+            continue
+        elif resp.lowe() == "N" or "n":
+            break
+        else:
+            print("invalid input")
+            break
+
+
