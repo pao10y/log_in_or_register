1379ea009175b9812da711b51f1d8abcde0d5f3c
iStunniq <78080961+iStunniq@users.noreply.github.com>
2021-01-31 23:57:00 +0800

    Update search product.py

--- search product.py
+++ search product.py
@@ -6,17 +6,16 @@ def findProd():
         if(n=='N' or n=='n'):
             print("Please enter item name:")
             i=input()
-            y=0
             for x in products:
-                y=y+1
-                if (i==products[y][1]):
-                    print(products[y])
+                if (i==x[1]):
+                    print(x)
                     print("Would you like to search for another item?")
                     d=input()
                     if(d=='Y' or d=='y'):
                         break
                     elif(d=='N' or d=='n'):
                         f=2
+                        break
                     else:
                         print("invalid")
                         break
@@ -25,17 +24,16 @@ def findProd():
         elif(n=='I' or n=='i'):
             print("Please enter item ID:")
             i=input()
-            y=0
             for x in products:
-                y=y+1
-                if(i==products[y][0]):
-                    print(products[y])
+                if(i==x[0]):
+                    print(x)
                     print("Would you like to search for another item?(Y or N)")
                     d=input()
                     if(d=='Y' or d=='y'):
                         break
                     elif(d=='N' or d=='n'):
                         f=2
+                        break
                     else:
                         print("invalid")
                         break
