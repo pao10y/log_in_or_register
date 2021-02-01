eea1f8f7fa0b2ea6afff5715a344a2dae860fdaa
christianbanez <78152023+christianbanez@users.noreply.github.com>
2021-02-01 22:29:36 +0800

    Add files via upload

---
+++ sales history.py
@@ -0,0 +1,32 @@
+def view_salesHistory():
+    print("---------SALES HISTORY---------")
+    while True:
+    #selecting option
+         print("1. View Products Sold")
+         print("2. View Product List")
+         print("3. Exit")
+         ch = input()
+         if ch == '1'
+             #code for displaying products sold
+             print("This function is not yet available")
+         elif ch == '2'
+             #displaying updated list
+             listProd()
+         elif ch == '3'
+             break
+         else:
+             print("Invalid Input")
+             continue
+     #(Undecided)code for sale total
+            
+
+
+
+
+
+
+
+
+
+    
+    
