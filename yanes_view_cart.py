de11a31b9a3a249237f1848d49447b704401e716
pao10y <57564147+pao10y@users.noreply.github.com>
2021-02-10 16:33:57 +0800

    Update yanes_view_cart.py

--- yanes_view_cart.py
+++ yanes_view_cart.py
@@ -307,7 +307,7 @@ def add_to_cart(user):
         input_item = input("Please Enter a Valid Product ID: ")
         for idx, item in enumerate(products):
             if input_item.upper() == products[idx][0]:
-                input_itemno = int(input("How many product do you want to add to cart: "))
+                input_itemno = int(input("How many do you want to add to cart: "))
                 if input_itemno <= products[idx][3]:
                     products[idx][3] = products[idx][3] - input_itemno
                     newcart = [products[idx][0],products[idx][1],input_itemno]
